5ee4367a6e05f61d2affa519b14067d0154ba6d4
hadrianhu888 <hadrian.hu@gmail.com>
2023-01-29 17:52:40 -0500

    inserted assertEquals to TestCalculator.java

--- src/TestCalculator.java
+++ src/TestCalculator.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 
@@ -9,29 +10,34 @@ public class TestCalculator {
         Calculator calculator = new Calculator();
         int result = calculator.Sum(10, 5);
         assert result == 15;
+        assertEquals(result, calculator.Sum(10, 5));
     }
     @Test
     public void testSub() {
         Calculator calculator = new Calculator();
         int result = calculator.Sub(10, 5);
         assert result == 5;
+        assertEquals(result, calculator.Sub(10, 5));
     }
     @Test
     public void testMul() {
         Calculator calculator = new Calculator();
         int result = calculator.Mul(10, 5);
         assert result == 50;
+        assertEquals(result,calculator.Mul(10,5));
     }
     @Test
     public void testDiv() {
         Calculator calculator = new Calculator();
         int result = calculator.Div(10, 5);
         assert result == 2;
+        ssertEquals(result, calculator.Div(10, 5));
     }
     @Test
     public void testMod() {
         Calculator calculator = new Calculator();
         int result = calculator.mod(10, 5);
+        ssertEquals(result, calculator.Mod(10, 5));
         assert result == 0;
     }
 }
